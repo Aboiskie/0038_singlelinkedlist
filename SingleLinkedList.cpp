246d6f045af0e6c766eb02316829b4879cacfac2
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-23 16:01:08 +0700

    menambahkan metode traverse untuk menampilkan data dalam LinkedList

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -92,3 +92,22 @@ public:
         delete current;
         return true;
     }
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData dalam list adalah: \n";
+            Node *currentNode = START;
+            
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << " ";
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
